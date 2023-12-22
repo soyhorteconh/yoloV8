882536453810de689fdffc5c91bb7c4ab32e0f8e
Horte Ramírez <114839269+soyhorteconh@users.noreply.github.com>
2023-12-22 16:06:08 +0100

    Update test1.py

--- yolov8/test1.py
+++ yolov8/test1.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 # run model with a video
 model = YOLO('/root/yolov8/model3.pt')
-results = model.track(source="/root/yolov8/dishwasher3.mp4",  save=True)
+results = model.track(source="/root/yolov8/dishwasher3.mp4",  show=True, save=True)
 results.save()
 
